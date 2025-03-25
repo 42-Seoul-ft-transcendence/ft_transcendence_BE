844acfeae31584020b51b91c811a35bec86c77bd
Ares-10 <cholionking1@naver.com>
2025-03-25 19:04:57 +0900

    init: 프로젝트 생성

---
+++ src/main.ts
@@ -0,0 +1,20 @@
+import Fastify from "fastify";
+
+const fastify = Fastify()
+
+fastify.get('/ping', async (request, reply) => {
+	return 'pong\n'
+})
+
+const start = async () => {
+	try {
+		await fastify.listen({ port: 8083 })
+		console.log('Server Start!!')
+	}
+	catch (error) {
+		fastify.log.error(error)
+		process.exit(1)
+	}
+}
+
+start()
