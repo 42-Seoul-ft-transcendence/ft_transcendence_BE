b6477a36856d99d5ba66f9daa1cb312d3b315082
Ares-10 <cholionking1@naver.com>
2025-04-27 16:16:05 +0900

    chore: 토큰 유효기간 증가

--- src/global/config/index.ts
+++ src/global/config/index.ts
@@ -3,8 +3,8 @@ export const GOOGLE_USERINFO_URL = 'https://www.googleapis.com/oauth2/v3/userinf
 
 // JWT 관련 설정
 export const JWT_SECRET = process.env.JWT_SECRET as string;
-export const ACCESS_TOKEN_EXPIRES_IN = '1h';
-export const REFRESH_TOKEN_EXPIRES_IN = '7d';
+export const ACCESS_TOKEN_EXPIRES_IN = '7d';
+export const REFRESH_TOKEN_EXPIRES_IN = '30d';
 
 export const ENCRYPTION_KEY = process.env.ENCRYPTION_KEY as string;
 
